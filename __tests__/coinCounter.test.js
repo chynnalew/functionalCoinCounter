be3d33383ecc9248ab30bbf071157ff19c466d07
chynnalew <chynnalew@yahoo.com>
2021-11-08 13:55:21 -0800

    pass all tests
    Co-Authored-by: Paige Tiedeman <paigetiedeman@gmail.com>

--- __tests__/coinCounter.test.js
+++ __tests__/coinCounter.test.js
@@ -1,23 +1,30 @@
 import counter from "../src/scripts";
 
 describe('counter', () => {
+  test('should recognize if NaN', () => {
+    expect(counter("hello")).toEqual("not a number");
+  });
   test('should recognize .01 and return 1 pennies', () => {
     expect(counter(.01)).toEqual("1 pennies");
   });
 
   test('should return amount of quarters needed', () => {
-    expect(counter(.50)).toEqual("2 quarters");
+    expect(counter(.50)).toEqual("2 quarters ");
   });
 
-  test('should return 1 quarter if the change is 30 cents', () => {
-    expect(counter(.30)).toEqual("1 quarters");
+  test('should return 1 quarters 1 nickles if the change is 30 cents', () => {
+    expect(counter(.30)).toEqual("1 quarters 1 nickles ");
   });
 
   test('should return amount of dimes needed', () => {
-    expect(counter(.20)).toEqual("2 dimes");
+    expect(counter(.20)).toEqual("2 dimes ");
   });
 
   test('should return amount of nickles needed', () => {
-    expect(counter(.06)).toEqual("1 nickles");
+    expect(counter(.05)).toEqual("1 nickles ");
+  });
+
+  test('should return 1 dimes 1 nickles and 1 pennies if the change is 16 cents', () => {
+    expect(counter(.16)).toEqual("1 dimes 1 nickles 1 pennies");
   });
 });
--- src/scripts.js
+++ src/scripts.js
@@ -3,13 +3,20 @@
 // pennies, nickle, dimes, quarters
 
 export default function counter(amount) {
+  if (isNaN(amount)) {
+    return "not a number";
+  }
+  if (amount <= 0) {
+    return "";
+  }
   if (amount >= .25) {
-    return Math.floor(amount / .25) + " quarters";
-  } else if (amount >= .10) {
-    return Math.floor(amount/ .10) + " dimes";
-  } else if (amount >= .05) {
-    return Math.floor(amount / .05) + " nickles";
-  }else if(amount % .05 !== 0) {
-    return (amount % .05) * 100 + " pennies";
-  } 
+    return (Math.floor(amount / .25) + " quarters ").concat(counter((amount % .25).toFixed(2)));
+  }
+  if (amount >= .10) {
+    return (Math.floor(amount / .10) + " dimes ").concat(counter(amount % .10));
+  }
+  if (amount >= .05) {
+    return (Math.floor(amount / .05) + " nickles ").concat(counter(amount % .05));
+  }
+  return Math.round((amount * 100)) + " pennies";
 }
